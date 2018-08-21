017d1b4ddd983ac11ee4aef4dec69d6fec34c2af
Jonathan Hong <jonhong777@gmail.com>
2018-08-21 23:53:01 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -13,7 +13,7 @@ class FiguresController < ApplicationController
     end
     if params["figure"]["landmark_ids"] != nil
       params["figure"]["title_ids"].each do |id|
-        figure.titles << Title.find_by_id(id)
+        figure.titles << Landmark.find_by_id(id)
       end
     end
   end
