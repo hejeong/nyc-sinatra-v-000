349f93444df9d1501b4adc62811c8d13fd760c7e
Jonathan Hong <jonhong777@gmail.com>
2018-08-21 23:10:23 +0000

    Old code that is not working

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class FiguresController < ApplicationController
   get '/figures/new' do
     erb :'figures/new'
@@ -10,13 +11,15 @@ class FiguresController < ApplicationController
         @figure.titles << Title.find_by_id(id)
       end
     else
-
+      @figure.titles << Title.create(params["title"])
     end
-
     if !params["figure"]["landmark_ids"].empty?
       params["figure"]["landmark_ids"].each do |id|
         @figure.landmarks << Landmark.find_by_id(id)
       end
+    else
+      @figure.landmarks << Landmark.create(params["landmark"])
     end
+    @figure.save
   end
 end
