c5db6df6467277250355b23c3c6f3eb3eb676aed
Jonathan Hong <jonhong777@gmail.com>
2018-08-21 23:26:59 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -5,21 +5,6 @@ class FiguresController < ApplicationController
   end
 
   post '/figures' do
-    @figure = Figure.create(name: params["figure"]["name"])
-    if !params["figure"]["title_ids"].empty?
-      params["figure"]["title_ids"].each do |id|
-        @figure.titles << Title.find_by_id(id)
-      end
-    else
-      @figure.titles << Title.create(params["title"])
-    end
-    if !params["figure"]["landmark_ids"].empty?
-      params["figure"]["landmark_ids"].each do |id|
-        @figure.landmarks << Landmark.find_by_id(id)
-      end
-    else
-      @figure.landmarks << Landmark.create(params["landmark"])
-    end
-    @figure.save
+  
   end
 end
