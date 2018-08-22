d0a694494f7abffc1b194b87546ea08ac92eb36a
Jonathan Hong <jonhong777@gmail.com>
2018-08-22 01:38:58 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -11,4 +11,9 @@ class LandmarksController < ApplicationController
     @landmarks = Landmark.all
     erb :'landmarks/index'
   end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find_by_id(params[:id])
+    erb :show
+  end
 end
